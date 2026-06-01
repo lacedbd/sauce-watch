9f92976961132ef2eef9d36509d356ba01fa0b1e
lacedbd <siamsledvk@gmail.com>
2026-06-01 08:59:47 +0600

    Switch from Tailwind Play CDN to static compiled CSS for smart TV compatibility

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./*.html"],
+  darkMode: 'class',
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ['Inter', 'system-ui', 'sans-serif'],
+      },
+    },
+  },
+  plugins: [],
+}
